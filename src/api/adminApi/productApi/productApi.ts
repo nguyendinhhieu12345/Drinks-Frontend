151470d6ac061676dad785582bfb99cc2ffe04ef
nguyendinhhieu12345 <dinhhieunguyen07@gmail.com>
2024-05-22 16:42:04 +0700

    feat: update product of branch

--- src/api/adminApi/productApi/productApi.ts
+++ src/api/adminApi/productApi/productApi.ts
@@ -26,13 +26,14 @@ export const getAllProduct = async (
     key?: string,
     pageCurrent?: number,
     productStatus?: string,
-    categoryId?: string
+    categoryId?: string,
+    branchId?: string
 ) => {
     try {
         const res = await httpRequest.get(
-            `/product?page=${pageCurrent}&size=10${productStatus !== "" ? `&productStatus=${productStatus}` : ""
+            `/product?page=${pageCurrent}&size=10${productStatus !== "" ? `&product_status=${productStatus}` : ""
             }${key !== "" ? `&key=${key}` : ""}${categoryId !== "" ? `&categoryId=${categoryId}` : ""
-            } `
+            }${branchId ? `&branch_id=${branchId}` : ""}`
         );
         return res;
     } catch (error) {
@@ -72,6 +73,15 @@ export const updateProduct = async (formData: FormData, ProductId: string, type:
         const res = await httpRequest.put(`/product/${ProductId}?product_type=${type}`, formData, {
             "Content-Type": "multipart/form-data",
         });
+        return res;
+    } catch (error) {
+        return Promise.reject(error);
+    }
+};
+
+export const updateStatusBranchProduct = async (productId: string, branchId: string, branch_product_status: string) => {
+    try {
+        const res = await httpRequest.patch(`/product/${productId}/branch/${branchId}?branch_product_status=${branch_product_status}`);
         return res;
     } catch (error) {
         return Promise.reject(error);
